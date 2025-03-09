440762a54351140582dc8eb6c7affafced2ea9a2
iuwyr <egor.nalivajko@yandex.ru>
2025-03-09 15:59:41 +0000

    changed code style and fixed conflicts

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
-int main(int argc, char** argv) { // new comment 1
+int
+main(int argc, char** argv) 
+{ // new comment 1
    std::string name;
    std::cout << "Enter your name" << std::endl; // new comment 2
-   std::cin >> name; // new comment 3
+   std::cin >> name;                            // new comment 3
    std::cout << "Hello world from @" << name << std::endl;
 }
+
