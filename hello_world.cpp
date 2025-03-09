566e73ca99bd22f8e428de6047508e6f7880ce7e
iuwyr <egor.nalivajko@yandex.ru>
2025-03-09 15:14:38 +0000

    added comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) { // comment1
    std::string name;
-   std::cout << "Enter your name" << std::endl;
-   std::cin >> name;
+   std::cout << "Enter your name" << std::endl; //comment2
+   std::cin >> name; //comment3
    std::cout << "Hello world from @" << name << std::endl;
 }
